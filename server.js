0776297b65433e210ed1c44d006304c3e35bd6bd
Robson Ferreira <tech.rob@icloud.com>
2021-05-26 19:33:47 -0400

    third commit

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const path = require("path");
 const axios = require("axios");
 const redis = require("redis");
-const fixer = require("fixer-api");
 const app = express();
+
+const localhost = "127.0.0.1";
+const client = redis.createClient({
+  host: localhost,
+  port: 6379,
+  //   password: "kybVcAA2dumxx1nOPuVscXFLF19klfol",
+});
+client.on("connect", () => console.log("connected to redis!!"));
+client.on("error", (err) => console.log(`Error ${err}`));
 
 const API_URL =
   "http://data.fixer.io/api/latest?access_key=5034058652fe84ccaefce8fc584ee219";
-// fixer.set({ accessKey: "5034058652fe84ccaefce8fc584ee219" });
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", {

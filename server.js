05bdab7b09dcbb4d9d7e561c25708d364074097e
Robson Ferreira <tech.rob@icloud.com>
2021-05-27 10:20:39 -0400

    added dockerfile, code in views, css

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const localhost = "127.0.0.1";
 const client = redis.createClient({
   host: localhost,
   port: 6379,
-  //   password: "kybVcAA2dumxx1nOPuVscXFLF19klfol",
 });
 client.on("connect", () => console.log("connected to redis!!"));
 client.on("error", (err) => console.log(`Error ${err}`));
@@ -26,12 +25,24 @@ app.get("/rate/:date", (req, res) => {
   const date = req.params.date;
   const url = `${API_URL}/${date}?base=USD`;
 
+  const countKey = `USD:${date}:count`;
+  const ratesKey = `USD:${date}:rates`;
+
+  client.incr(countKey, (err, count) => {
+    client.hgetall(ratesKey, (err, rates) => {
+      if (rates) return res.json({ rates, count });
+    });
+  });
+
   axios
     .get(url)
     .then((response) => {
-      return res.json({ rates: response.data.rates });
+      client.hmset(ratesKey, response.data.rates, (err, result) => {
+        if (err) console.log(err);
+      });
+      return res.json({ count, rates: response.data.rates });
     })
-    .catch((error) => console.log(console.log(error)));
+    .catch((error) => res.json(error.response.data));
 });
 
 const port = process.env.PORT || 8000;
